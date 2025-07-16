2425479dc376b955428bc3e57e1969843a2e506f
Tabrez Dal <tabrez.dal@gmail.com>
2025-07-16 21:23:13 +0530

    Updated YOE and Changed Hero Line

--- src/components/HeroSection/HeroSectionData.js
+++ src/components/HeroSection/HeroSectionData.js
@@ -4,10 +4,10 @@ export const HeroSectionData = {
   introStatingLine: "This is me",
   name: "Tabrez Dal",
   introLine:
-    "a Software Developer and Product Designer with over 4 years of experience, holding a Bachelor's Degree in Computer Science and Engineering.",
+    "a Software Engineer and Product Designer with over 5 years of experience, bridging pixels and code for best user experiences.",
   buttonData: {
     name: "Download CV",
-    url: "https://drive.google.com/file/d/1GN7y4_uQgm7XaMGmB_U52roY3tSuByPt/view?usp=sharing",
+    url: "https://drive.google.com/file/d/1Iz4mEuRfPz1hJb9AubC0aO-1UzP0n_ft/view?usp=drive_link",
     url2: "https://drive.google.com/file/d/1McjuJeJgD-oTfHLbVzt9VJztWN_cR3Vr/view?usp=sharing",
   },
   heroImage: ImageHelper.HeroImage,
